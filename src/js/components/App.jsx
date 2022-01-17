d101583b0abf3cc8614e0a890fb22d6330946093
Teemu Kakko <sormiteippi@windowslive.com>
2022-01-17 21:46:05 +0200

    Create player variables

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 
 /* Components */
+import PlayerInfo from './PlayerInfo/index.jsx';
 import ScoreCard from './ScoreCard/index.jsx';
 
 /* Styles */
@@ -10,8 +11,29 @@ const AppContainer = styled.div`
 `;
 
 const App = (props) => {
+  const [playerCount, setPlayerCount] = useState(0);
+  const [players, setPlayers] = useState([
+    { playerId: 0, name: '' },
+    { playerId: 1, name: '' },
+    { playerId: 2, name: '' },
+    { playerId: 3, name: '' }
+  ]);
+  const [scores, setScores] = useState([
+    { playerId: 0, a: 0, b: 0, c: 0 },
+    { playerId: 1, a: 0, b: 0, c: 0 },
+    { playerId: 2, a: 0, b: 0, c: 0 },
+    { playerId: 3, a: 0, b: 0, c: 0 }
+  ]);
+
+  function handlePlayers(name, playerId) {
+    console.log(name, playerId);
+  }
+
   return (
     <AppContainer>
+      <PlayerInfo
+        handlePlayers={ handlePlayers }>
+      </PlayerInfo>
       <ScoreCard/>
     </AppContainer>
   );
---
+++ src/js/components/PlayerInfo/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/* Styles */
+import * as s from './index.styled.js';
+
+const PlayerInfo = (props) => {
+  return (<s.Container>
+    <h1>PlayerInfo</h1>
+  </s.Container>);
+};
+
+export default PlayerInfo;
