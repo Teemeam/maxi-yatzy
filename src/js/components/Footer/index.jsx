7e3bb489f9e9d78ea0e1d4b3fe7f2a62e90fd4dc
Teemu Kakko <sormiteippi@windowslive.com>
2022-03-12 12:17:41 +0200

    Update footer

--- src/js/components/Footer/index.jsx
+++ src/js/components/Footer/index.jsx
@@ -4,7 +4,10 @@ import React from 'react';
 import * as s from './index.styled.js';
 
 export const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+
   return (<s.Container>
-    <p className='copyright'><a href='https://kakkoteemu.fi/' target='_blank'>&copy; 2022</a></p>
+    <p className='copyright'><a href='https://kakkoteemu.fi/' target='_blank'>&copy; { year }</a></p>
   </s.Container>);
 };
